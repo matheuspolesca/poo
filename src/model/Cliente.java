cca5317d3666580b869cfb6cc2bf797dafd17d3c
Gabriel Augusto <gabriel.ramos@ufvjm.edu.br>
2021-09-07 22:31:43 -0300

    [Ajustes]: Correções Javadoc

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -18,9 +18,23 @@ public class Cliente extends Pessoa implements Comparable <Cliente>{
     private IdTriagem idTriagem;
     private Date data;
 
+    /**
+     * Construtor padrão da classe
+     */
     public Cliente() {
     }
     
+    /**
+     * Construtor adicional da classe
+     * @param nome
+     * @param sobrenome
+     * @param idade
+     * @param telefone
+     * @param email
+     * @param endereco
+     * @param cpf
+     * @param sexo
+     */
     public Cliente (String nome, String sobrenome, int idade, String telefone, String email, String endereco, String cpf, String sexo)
     {
         super(nome, sobrenome, idade, telefone, email, endereco, cpf, sexo);
@@ -28,34 +42,66 @@ public class Cliente extends Pessoa implements Comparable <Cliente>{
         ClienteApplication.getNumClientes();
     }
     
+    /**
+     * Obtém o status do cliente
+     * @return string
+     */ 
     public String getStatus() {
         return status;
     }
 
+    /**  
+     * Define o status do cliente
+     * @param status
+     */
     public void setStatus(String status) {
         this.status = status;
     }
 
+    /**
+     * Obtém as doenças de um cliente
+     * @return ArrayList
+     */
     public ArrayList<Doenca> getClienteDoencas() {
         return clienteDoencas;
     }
 
+    /**
+     * Define as doenças de um cliente
+     * @param clienteDoencas
+     */
     public void setClienteDoencas(ArrayList<Doenca> clienteDoencas) {
         this.clienteDoencas = clienteDoencas;
     }
 
+    /**
+     * Obtém o id de triagem de um cliente
+     * @return
+     */
     public IdTriagem getIdTriagem() {
         return idTriagem;
     }
 
+    /**
+     * Define o id de triagem de um cliente
+     * @param idtriagem
+     */
     public void setIdTriagem(IdTriagem idtriagem) {
         this.idTriagem = idtriagem;
     }
     
+    /**
+     * Obtém a data 
+     * @return date
+     */
     public Date getData() {
         return data;
     }
     
+    /**
+     * Define a data
+     * @param data
+     */
     public void setData(Date data) {
         this.data = data;
     }
@@ -77,6 +123,11 @@ public class Cliente extends Pessoa implements Comparable <Cliente>{
         return texto;
     }
     
+    /**
+     *
+     * @param cliente
+     * @return
+     */
     @Override
     
     public int compareTo(Cliente cliente) {
